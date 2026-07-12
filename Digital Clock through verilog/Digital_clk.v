8ddea2c0b4733998eb8f98f5538cd09366a1af7d
denanath <denanaths.ec25@bitsathy.ac.in>
2026-07-12 21:44:52 +0530

    verilog file is updated

--- Digital Clock through verilog/Digital_clk.v
+++ Digital Clock through verilog/Digital_clk.v
@@ -25,7 +25,7 @@ output reg [3:0]sec_ones,sec_tens,min_ones,min_tens,hr_ones,hr_tens,day;
 input wire clk;
 input wire rst;
 
-/*localparam clk_freq = 100000000;
+localparam clk_freq = 100000000;
 
 reg [31:0] clock_count;
 reg one_second_enable;
@@ -51,7 +51,7 @@ begin
         one_second_enable<=0;
      end
  end
- end */ 
+ end 
  
 always @(posedge clk or posedge rst)
 begin
@@ -65,8 +65,9 @@ begin
         hr_tens<=0;
         day <= 0;
      end
-     //else if(one_second_enable)
-     else
+     
+    else if(one_second_enable)
+    
      begin
         sec_ones <= sec_ones + 1;
         if(sec_ones == 9)
@@ -106,4 +107,20 @@ begin
         end 
     end 
 end
+
+Display_controler display_unit (
+        .clk(clk),
+
+        .sec_ones(sec_ones),
+        .sec_tens(sec_tens),
+        .min_ones(min_ones),
+        .min_tens(min_tens),
+        .hr_ones(hr_ones),
+        .hr_tens(hr_tens),
+        .day(day),
+
+        .AN(AN),
+        .seg(seg)
+    );
+
 endmodule
